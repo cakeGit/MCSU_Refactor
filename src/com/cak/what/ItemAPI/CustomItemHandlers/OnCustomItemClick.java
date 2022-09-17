a36cca7185bd06dd06c6d2d34f44208ab6d7c3f5
cakeGit <65340665+cakeGit@users.noreply.github.com>
2022-09-17 12:49:33 +0100

    1.2.2 - ummm yes
    Signed-off-by: cakeGit <65340665+cakeGit@users.noreply.github.com>

---
+++ src/com/cak/what/ItemAPI/CustomItemHandlers/OnCustomItemClick.java
@@ -0,0 +1,5 @@
+package com.bonkle.whatever.ItemAPI.CustomItemHandlers;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public interface OnCustomItemClick { void run(InventoryClickEvent event); }
---
+++ src/com/cak/what/ItemAPI/CustomItemHandlers/OnCustomItemUse.java
@@ -0,0 +1,5 @@
+package com.bonkle.whatever.ItemAPI.CustomItemHandlers;
+
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public interface OnCustomItemUse { void run(PlayerInteractEvent event); }
